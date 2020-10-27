06c6d95c9f56ae1c0749ef8329e4cf13e4acbd71
bradleyhop <bradleyhop@gmail.com>
2020-10-27 16:15:06 -0500

    project seleton made

---
+++ src/components/BarChart.vue
@@ -0,0 +1,30 @@
+<script>
+// import * as d3 from 'd3';
+
+export default {
+  name: 'BarChart',
+
+  mounted() {
+    fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json');
+  },
+};
+</script>
+
+<template>
+  <p id="title" class="bar-chart-box">
+    Hello, world!
+  </p>
+</template>
+
+<style scoped lang="scss">
+.bar-chart-box {
+  background-color: lightgrey;
+  margin: auto;
+  width: 80vw;
+  height: 70vh;
+}
+
+.chart-title {
+  color: #000;
+}
+</style>
